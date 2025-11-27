18c3ec8d35e6f2cecfec2f89429f8c524b3711aa
nkrissetiyanto-png <nkrissetiyanto@me.com>
2025-11-28 00:38:38 +0700

    Update data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,12 +1,28 @@
 import yfinance as yf
 import pandas as pd
+import requests
 
-def load_candles(symbol, interval):
-    df = yf.download(
-        tickers=symbol,
-        period="5d",
-        interval=interval
-    )
-    df = df.reset_index()
-    df.columns = ["date", "open", "high", "low", "close", "adj", "volume"]
-    return df
+def load_candles(symbol, interval, mode="Crypto"):
+
+    if mode.startswith("Crypto"):
+        url = f"https://api.binance.com/api/v3/klines?symbol={symbol}&interval={interval}&limit=200"
+        data = requests.get(url).json()
+
+        df = pd.DataFrame(data)
+        df = df.iloc[:, :6]
+        df.columns = ["date", "open", "high", "low", "close", "volume"]
+        df["date"] = pd.to_datetime(df["date"], unit="ms")
+        df = df.astype({
+            "open": float,
+            "high": float,
+            "low": float,
+            "close": float,
+            "volume": float
+        })
+        return df
+
+    else:
+        df = yf.download(symbol, period="5d", interval=interval)
+        df = df.reset_index()
+        df.columns = ["date", "open", "high", "low", "close", "adj", "volume"]
+        return df
