6ab49160f9650fd05cb78cfc0e1b3c3bbf712b3c
nkrissetiyanto-png <nkrissetiyanto@me.com>
2025-12-08 14:49:56 +0700

    Update ai_memory.py

--- components/ai_memory.py
+++ components/ai_memory.py
@@ -1,1 +1,20 @@
+# components/ai_memory.py
+
+from collections import deque
+import time
+
+# Simpan max 200 data
+AI_HISTORY = deque(maxlen=200)
+
+def save_ai_result(result):
+    AI_HISTORY.append({
+        "time": time.time(),
+        "direction": result["direction"],
+        "confidence": result["confidence"],
+        "prob_up": result["prob_up"],
+        "prob_down": result["prob_down"]
+    })
+
+def get_history():
+    return list(AI_HISTORY)
 
