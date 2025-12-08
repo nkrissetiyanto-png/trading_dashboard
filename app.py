3c9c237ab7c24c30e75e969e2d86302050d24628
nkrissetiyanto-png <nkrissetiyanto@me.com>
2025-12-08 15:45:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from components.crypto_sentiment import render_crypto_sentiment
 from components.ai_signal import render_ai_signal
 from data.data_loader import load_candles
 from components.ai_confidence_chart import render_ai_confidence_chart
-from components.ai_reversal import render_ai_reversal
+#from components.ai_reversal import render_ai_reversal
 from components.reversal_detector import render_reversal_detector
 
 # ======================================================
