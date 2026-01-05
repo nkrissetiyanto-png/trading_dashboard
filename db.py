e331852a94187a96b7dd7a9e89ebc19e4c02d06e
nkrissetiyanto-png <nkrissetiyanto@me.com>
2026-01-05 16:02:26 +0700

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,20 @@
+import sqlite3
 
+DB_PATH = "users.db"
+
+def get_db():
+    return sqlite3.connect(DB_PATH, check_same_thread=False)
+
+def init_db():
+    conn = get_db()
+    c = conn.cursor()
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE,
+            password_hash TEXT,
+            plan TEXT
+        )
+    """)
+    conn.commit()
+    conn.close()
